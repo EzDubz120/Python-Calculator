edcf0caae272d2d521a3346efb81e5fc18303414
EzDubz120 <79054404+EzDubz120@users.noreply.github.com>
2021-05-21 19:03:46 +0100

    Add files via upload

---
+++ Pyton_calculator.py
@@ -0,0 +1,19 @@
+import time
+op = input("enter operator ")
+time.sleep(1)
+number1 = float(input("enter the first number "))
+number2 = float(input("enter the second number "))
+
+if op == "+":
+    print(number1 + number2)
+elif op == "-":
+    print(number1 - number2)
+elif op == "*":
+    print(number1 * number2)
+elif op == "x":
+    print(number1 * number2)
+elif op == "/":
+    print(number1 / number2)
+else:
+    print("invalid operator")
+    print("please enter a valid operator")
